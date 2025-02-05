f86d1bf969c0a39b834ebbba42d59beab8b2e7a6
viniciusferreira999 <viniciusbritoferreira13@gmail.com>
2025-02-05 15:23:37 -0300

    novas funções

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const sexo = document.getElementById("sexo");
     const quadril = document.getElementById("quadril");
     const extraMedidas = document.getElementById("extra-medidas");
-
+    const idade = document.getElementById('idade')
+    
     // Exibir campo do quadril apenas para mulheres
     sexo.addEventListener("change", function () {
         if (sexo.value === "feminino") {
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const altura = parseFloat(document.getElementById("altura").value);
         const cintura = parseFloat(document.getElementById("cintura").value);
         const pescoco = parseFloat(document.getElementById("pescoco").value);
+        const idade = parseFloat(document.getElementById("idade").value);
         const quadrilValor = sexo.value === "feminino" ? parseFloat(quadril.value) : 0;
         
         let gordura;
@@ -29,9 +31,27 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             gordura = 163.205 * Math.log10(cintura + quadrilValor - pescoco) - 97.684 * Math.log10(altura) - 78.387;
         }
+        
+        
+        gordura += (idade * 0.1); 
+        
+        gordura = gordura.toFixed(2); 
+        
+        document.getElementById("resultado-texto").innerHTML = `Sua gordura corporal é aproximadamente <strong>${gordura}%</strong>.`;
 
-        gordura = gordura.toFixed(2); // Arredonda para 2 casas decimais
+    // Atualiza a largura da barra com base no percentual
+    const barra = document.getElementById("barra-progresso");
+    barra.style.width = `${gordura}%`;
 
-        document.getElementById("resultado").innerHTML = `Sua gordura corporal é aproximadamente <strong>${gordura}%</strong>.`;
+    
+    if (gordura < 15) {
+    barra.style.backgroundColor = "#4caf50"; // Verde (baixo)
+    } else if (gordura < 25) {
+    barra.style.backgroundColor = "#ff9800"; // Laranja (médio)
+    }   else {
+    barra.style.backgroundColor = "#f44336"; // Vermelho (alto)
+    }
+
     });
 });
+
